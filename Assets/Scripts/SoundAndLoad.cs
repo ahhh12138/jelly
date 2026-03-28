188840a6d490e30266d405e81d6a7befb294d5bd
Inujiro-Kalpas <inujiro_950301@qq.com>
2026-03-28 08:13:06 +0800

    Jelly1.7
    可执行程序在jelly文件夹

--- Assets/Scripts/SoundAndLoad.cs
+++ Assets/Scripts/SoundAndLoad.cs
@@ -6,23 +6,32 @@ public class SoundAndLoad : MonoBehaviour
     public AudioClip clickSound;
     private AudioSource audioSource;
 
-    void Start()
+    private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        if (audioSource != null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+        DontDestroyOnLoad(gameObject);
     }
+    //void Start()
+    //{
+    //    audioSource = GetComponent<AudioSource>();
+    //}
 
     // 开始游戏
     public void PlaySoundAndStart()
     {
         PlaySound();
-        Invoke("LoadMainScene", 0.3f); // 👈 延迟 0.3 秒！
+        Invoke("LoadMainScene", 0.1f); // 延迟 0.1 秒！
     }
 
     // 重新开始
     public void PlaySoundAndRestart()
     {
         PlaySound();
-        Invoke("LoadStartScene", 0.3f); // 👈 延迟 0.3 秒！
+        Invoke("LoadStartScene", 0.1f); // 延迟 0.1秒！
     }
 
     void PlaySound()
